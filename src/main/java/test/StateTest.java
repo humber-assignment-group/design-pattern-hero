a0bc7bdf019669b229f5fde63f4388c10fec6c6c
jiyunbaik <58647409+jiyunbaik@users.noreply.github.com>
2021-04-18 19:53:23 -0400

    Update StateTest.java

--- src/main/java/test/StateTest.java
+++ src/main/java/test/StateTest.java
@@ -1,18 +1,31 @@
 package test;
 
+import com.github.humbergroup.Hero;
+import com.github.humbergroup.state.NormalState;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StateTest {
 
     @org.junit.jupiter.api.Test
-    void initialState() {
+    void normalStateTest() {
+        Hero hero = new Hero("tester", 100, 1, 10);
+        hero.beAttack(40);
+        assertEquals(60, hero.getRestHp());
+        System.out.println(hero.getHp());
+        System.out.println(hero.getRestHp());
+        System.out.println(hero.getHpPercent());
+        System.out.println(hero.getState());
+//        assertTrue(hero.getState() instanceof NormalState);
     }
 
     @org.junit.jupiter.api.Test
-    void transitionState() {
+    void crazyStateTest() {
+
     }
 
     @org.junit.jupiter.api.Test
-    void beAttacked() {
+    void deathStateTest() {
+
     }
 }
