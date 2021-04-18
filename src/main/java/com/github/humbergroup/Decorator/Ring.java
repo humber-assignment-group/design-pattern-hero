99b8cd0a528c79ac4bf37507057b2b7112e82ba9
Sandeep626 <singh.sandeep.0094@gmail.com>
2021-04-18 16:26:02 -0400

    extendDecorator

--- src/main/java/com/github/humbergroup/Decorator/Ring.java
+++ src/main/java/com/github/humbergroup/Decorator/Ring.java
@@ -2,13 +2,19 @@ package com.github.humbergroup.Decorator;
 
 import com.github.humbergroup.Hero;
 
-public class Ring {
+public class Ring extends HeroDecorator{
 
     public Ring (Hero hero){
+        super(hero);
         hero.increaseHp(10);
     }
 
     public void render() {
+
+    }
+
+    @Override
+    public void beAttack(int hp) {
 
     }
 }
--- src/main/java/com/github/humbergroup/Decorator/Sword.java
+++ src/main/java/com/github/humbergroup/Decorator/Sword.java
@@ -2,14 +2,20 @@ package com.github.humbergroup.Decorator;
 
 import com.github.humbergroup.Hero;
 
-public class Sword {
+public class Sword extends HeroDecorator{
 
     public Sword (Hero hero){
+        super(hero);
         hero.increaseDefence(2);
         hero.increaseAttack(10);
     }
 
     public void render() {
+
+    }
+
+    @Override
+    public void beAttack(int hp) {
 
     }
 }
