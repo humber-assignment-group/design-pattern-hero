818ee9af511cf3493764f2038ee1185815876ddb
jiyunbaik <58647409+jiyunbaik@users.noreply.github.com>
2021-04-18 19:35:36 -0400

    Create StateTest.java

---
+++ src/main/java/test/StateTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StateTest {
+
+    @org.junit.jupiter.api.Test
+    void initialState() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void transitionState() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void beAttacked() {
+    }
+}
