802fe5ac06779da4ec7d2b253a4462339cc68898
Sandeep626 <singh.sandeep.0094@gmail.com>
2021-04-18 16:07:17 -0400

    change to intellj

---
+++ src/main/java/com/github/humbergroup/Decorator/HeroDecorator.java
@@ -0,0 +1,22 @@
+package com.github.humbergroup.Decorator;
+
+import com.github.humbergroup.Hero;
+import com.github.humbergroup.Role;
+
+public abstract class HeroDecorator implements Role {
+	
+	Hero hero;
+	
+	public HeroDecorator (Hero hero) {
+		this.hero = hero;
+	}
+	
+	public void render() {
+		
+	}
+	
+	public void beAttacked(int hp) {
+		
+	}
+
+}
