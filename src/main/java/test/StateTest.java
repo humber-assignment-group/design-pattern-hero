57a5caf8dbed134c1f37d77babbd61e7b5f62f49
jiyunbaik <58647409+jiyunbaik@users.noreply.github.com>
2021-04-18 20:01:35 -0400

    Update StateTest.java

--- src/main/java/test/StateTest.java
+++ src/main/java/test/StateTest.java
@@ -1,7 +1,10 @@
 package test;
 
 import com.github.humbergroup.Hero;
+import com.github.humbergroup.state.CrazyState;
+import com.github.humbergroup.state.DeathState;
 import com.github.humbergroup.state.NormalState;
+import com.github.humbergroup.state.State;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,20 +15,27 @@ class StateTest {
         Hero hero = new Hero("tester", 100, 1, 10);
         hero.beAttack(40);
         assertEquals(60, hero.getRestHp());
-        System.out.println(hero.getHp());
-        System.out.println(hero.getRestHp());
-        System.out.println(hero.getHpPercent());
-        System.out.println(hero.getState());
-//        assertTrue(hero.getState() instanceof NormalState);
+        assertTrue(hero.getState() instanceof NormalState);
     }
 
     @org.junit.jupiter.api.Test
     void crazyStateTest() {
-
+        Hero hero = new Hero("tester", 100, 1, 10);
+        hero.beAttack(95);
+        assertEquals(5, hero.getRestHp());
+        System.out.println(hero.getHpPercent());
+        System.out.println(hero.getState());
+        assertTrue(hero.getState() instanceof CrazyState);
     }
 
     @org.junit.jupiter.api.Test
     void deathStateTest() {
-
+        Hero hero = new Hero("tester", 100, 1, 10);
+        hero.beAttack(100);
+        assertEquals(0, hero.getRestHp());
+        System.out.println(hero.getHpPercent());
+        System.out.println(hero.getState());
+        System.out.println(State.InitialState(hero));
+        assertTrue(hero.getState() instanceof DeathState);
     }
 }
