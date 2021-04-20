75f65a7a42246f64e55530e801f08ce27f070730
jiyunbaik <58647409+jiyunbaik@users.noreply.github.com>
2021-04-19 21:16:09 -0400

    Update FactoryTest.java

--- src/main/java/com/github/humbergroup/testing/FactoryTest.java
+++ src/main/java/com/github/humbergroup/testing/FactoryTest.java
@@ -11,8 +11,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FactoryTest {
 
     @Test
-    public void test(){
+    public void test1(){
         HeroName name = HeroName.Zyra;
+        Hero hero = HeroFactory.getInstance().create(name);
+        assertEquals(name.name(), hero.getName());
+        assertEquals(name.getHp(), hero.getHp());
+        assertEquals(name.getDefence(), hero.getDefence());
+        assertEquals(name.getAttack(), hero.getAttack());
+    }
+
+    @Test
+    public void test2(){
+        HeroName name = HeroName.Anivia;
+        Hero hero = HeroFactory.getInstance().create(name);
+        assertEquals(name.name(), hero.getName());
+        assertEquals(name.getHp(), hero.getHp());
+        assertEquals(name.getDefence(), hero.getDefence());
+        assertEquals(name.getAttack(), hero.getAttack());
+    }
+
+    @Test
+    public void test3(){
+        HeroName name = HeroName.Blitzcrank;
         Hero hero = HeroFactory.getInstance().create(name);
         assertEquals(name.name(), hero.getName());
         assertEquals(name.getHp(), hero.getHp());
